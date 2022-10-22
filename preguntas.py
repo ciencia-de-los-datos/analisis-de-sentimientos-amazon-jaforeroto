f53185c1a13ac03f959a9390231d8a9fe671e81f
Javier_Forero <112499361+jaforeroto@users.noreply.github.com>
2022-10-22 18:59:22 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -115,7 +115,7 @@ def pregunta_04():
         analyzer=analyzer,
         lowercase=True,
         stop_words="english",
-        token_pattern=r"(?u)\b[a-zA-Z][a-zA-Z]+\b",
+        # token_pattern=r"(?u)\b[a-zA-Z][a-zA-Z]+\b",
         binary=True,
         max_df=1.0,
         min_df=5,
